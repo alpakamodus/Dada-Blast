2eac1dd57747931429722545f5c855769ede399e
alpakamodus <michael.hoellrigl@hotmail.com>
2026-06-22 10:40:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ canvas.width = window.innerWidth - 30;
 canvas.height = window.innerHeight - 30;
 
 ctx.fillRect(canvas.width/2-canvas.width/4,canvas.height/2-canvas.height/4,canvas.width/2,canvas.height/2);
+
+let x;
+let y;
+
+canvas.addEventListener("pointerdown", (e)=> {
+x = e.clientX;
+y = e.clientY;
+});
+while(1){
+    setInterval(gameLoop, 4);
+}
+function gameLoop(){
+    currentTime = performance.now();
+    dt = (currentTime - lastTime) / 16.6667;
+    lastTime = currentTime;
+    update(dt);
+    draw();
+    requestAnimationFrame(gameLoop());
+}
+
+function update(dt){
+
+}
+function draw(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillRect(canvas.width/2-canvas.width/4,canvas.height/2-canvas.height/4,canvas.width/2,canvas.height/2);
+}
