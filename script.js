222020ae8c3ae0ac3cbb10dc5679829a4eb6b0c1
alpakamodus <michael.hoellrigl@hotmail.com>
2026-06-22 11:40:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 30;
-canvas.height = window.innerHeight - 30;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 let x = canvas.width/2-canvas.width/4;
 let y = canvas.height/2-canvas.height/4;
