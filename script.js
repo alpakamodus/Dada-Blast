1b3c7bd5ca101ee32aff4a308379c68570662e65
alpakamodus <michael.hoellrigl@hotmail.com>
2026-06-25 17:43:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("myCanvas"); 
+const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -202,8 +202,8 @@ function checkForSnap(b) {
           blockSize / 2.1
       ) {
         if (checkIfFree(b, x, y)) {
-          for (let h = 0; h < blockLib[b.Id].length && y + h < 8; h++) {
-            for (let w = 0; w < blockLib[b.Id][0].length && x + w < 8; w++) {
+          for (let h = 0; h < blockLib[b.Id].length; h++) {
+            for (let w = 0; w < blockLib[b.Id][0].length; w++) {
               if (blockLib[b.Id][h][w] == true) {
                 grid[y + h][x + w] = true;
               }
